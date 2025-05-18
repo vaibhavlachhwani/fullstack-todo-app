3d90ec4484f171b75346b68ec8d4bbeedd5aeb8d
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-05-18 15:15:48 +0530

    improve basic authentication

---
+++ backend/todo-rest-api/src/main/java/com/vaibhav/todorestapi/config/BasicAuthSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.vaibhav.todorestapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class BasicAuthSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(
+                        (request) -> {
+                            request
+                                    .anyRequest()
+                                    .authenticated();
+                        }
+                )
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(
+                        (session) -> {
+                            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                        }
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
+
--- backend/todo-rest-api/src/main/java/com/vaibhav/todorestapi/exception/Error.java
+++ backend/todo-rest-api/src/main/java/com/vaibhav/todorestapi/exception/Error.java
@@ -1,5 +1,6 @@
 package com.vaibhav.todorestapi.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,7 +9,11 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 public class Error {
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private LocalDateTime timestamp;
+
     private String message;
+
     private String details;
 }
